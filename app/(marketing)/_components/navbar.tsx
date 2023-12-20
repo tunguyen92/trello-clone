b0bdebacfed6b6591e3fb02302e01de3f8e06670
Kantee <tunguyen3692@gmail.com>
2023-12-20 15:24:12 +0700

    deployment fix import

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -1,6 +1,7 @@
-import Logo from "@/components/logo";
-import { Button } from "@/components/ui/button";
 import Link from "next/link";
+
+import { Logo } from "@/components/logo";
+import { Button } from "@/components/ui/button";
 
 export const Navbar = () => {
   return (
